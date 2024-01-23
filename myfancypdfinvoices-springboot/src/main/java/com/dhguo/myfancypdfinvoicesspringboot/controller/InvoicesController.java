4018cc0de12ff893add540f6156c4fd66cf03390
Haoyang Guo <hyg.haoyang@gmail.com>
2024-01-22 18:25:49 -0800

    rebuild app using spring boot

---
+++ myfancypdfinvoices-springboot/src/main/java/com/dhguo/myfancypdfinvoicesspringboot/controller/InvoicesController.java
@@ -0,0 +1,33 @@
+package com.dhguo.myfancypdfinvoicesspringboot.controller;
+
+import com.dhguo.myfancypdfinvoicesspringboot.dto.InvoiceDto;
+import com.dhguo.myfancypdfinvoicesspringboot.model.Invoice;
+import com.dhguo.myfancypdfinvoicesspringboot.service.InvoiceService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class InvoicesController {
+
+    private final InvoiceService invoiceService;
+
+    public InvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/invoices")
+    // @RequestMapping(value = "/invoices", method = RequestMethod.GET)
+    public List<Invoice> invoices() {
+        return invoiceService.findAll();
+    }
+
+    @PostMapping("/invoices")
+    public Invoice createInvoice(@Valid @RequestBody InvoiceDto invoiceDto) {
+        return invoiceService.create(invoiceDto.getUserId(), invoiceDto.getAmount());
+    }
+}
---
+++ myfancypdfinvoices-springboot/src/test/java/com/dhguo/myfancypdfinvoicesspringboot/MyfancypdfinvoicesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhguo.myfancypdfinvoicesspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfancypdfinvoicesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
