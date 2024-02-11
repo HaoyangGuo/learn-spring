24aed1ccb60f8a6ddff7b0a60ec7ce7fc6dfacf6
Haoyang Guo <hyg.haoyang@gmail.com>
2024-02-10 19:59:45 -0800

    test new file

--- spring-ioc-basics/src/main/java/com/dhguo/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ spring-ioc-basics/src/main/java/com/dhguo/myfancypdfinvoices/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -5,13 +5,13 @@ import com.dhguo.myfancypdfinvoices.service.InvoiceService;
 import com.dhguo.myfancypdfinvoices.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(basePackageClasses = ApplicationLauncher.class)
+@PropertySource("classpath:application.properties")
+@PropertySource(value = "classpath:/application-${spring.profiles.active}.properties"
+        , ignoreResourceNotFound = true)
 public class MyFancyPdfInvoicesApplicationConfiguration {
 
     @Bean
--- spring-ioc-basics/src/main/java/com/dhguo/myfancypdfinvoices/service/InvoiceService.java
+++ spring-ioc-basics/src/main/java/com/dhguo/myfancypdfinvoices/service/InvoiceService.java
@@ -2,7 +2,10 @@ package com.dhguo.myfancypdfinvoices.service;
 
 import com.dhguo.myfancypdfinvoices.model.Invoice;
 import com.dhguo.myfancypdfinvoices.model.User;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,13 +13,27 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @Component
 public class InvoiceService {
-//    @Autowired
-    private UserService userService;
+    private final UserService userService;
+    private final String cdnUrl;
+
 
     List<Invoice> invoices = new CopyOnWriteArrayList<>();
 
-    public InvoiceService(UserService userService) {
+    public InvoiceService(UserService userService, @Value("${cdn.url}") String cdnUrl) {
         this.userService = userService;
+        this.cdnUrl = cdnUrl;
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Fetching PDF Template from S3...");
+        // TODO download from s3 and save locally
+    }
+
+    @PreDestroy
+    public void shutdown() {
+        System.out.println("Deleting downloaded templates...");
+        // TODO actual deletion of PDFs
     }
 
     public List<Invoice> findAll() {
@@ -30,13 +47,8 @@ public class InvoiceService {
         }
 
         // TODO real pdf creation and storing it on network server
-        Invoice invoice = new Invoice(userId, "http://www.africau.edu/images/default/sample.pdf", amount);
+        Invoice invoice = new Invoice(userId, cdnUrl + "/images/default/sample.pdf", amount);
         invoices.add(invoice);
         return invoice;
-    }
-
-    @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
     }
 }
